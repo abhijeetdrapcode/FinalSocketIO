3fa2fc8eeb707ed1cc4218bd3bd6d36348f618bf
abhijeetdrapcode <abhijeet@drapcode.com>
2024-05-14 13:24:13 +0530

    Made a function that takes input for which events to store in the database

--- backend/config/mongoose.js
+++ backend/config/mongoose.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
 
-mongoose
-  .connect('mongodb://localhost:27017/UserClick')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB:', err));
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/UserClick');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 module.exports = mongoose;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const clickDataController = require('../controllers/clickDataController');
-
 const router = express.Router();
 
 module.exports = (io) => {
   io.on('connection', (socket) => {
     console.log('A user connected');
 
-    socket.on('clickData', (data) => {
-      console.log('Received click data:', data);
-      const req = socket.request;
-      clickDataController.saveClickData(data, req);
+    socket.on('clickData', async (data) => {
+      try {
+        console.log('Received click data:', data);
+        const req = socket.request;
+        const savedData = await clickDataController.saveClickData(data, req);
+        socket.emit('clickDataSaved', { message: 'Click data saved successfully', data: savedData });
+      } catch (err) {
+        console.error('Error saving click data:', err);
+        socket.emit('clickDataError', { message: err.message });
+      }
+    });
+
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
     });
   });
 
